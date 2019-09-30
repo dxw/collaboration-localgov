d49d9efeeae997bd5a2ef90c48bca8254a5ca8ab
Agz <agz@dxw.com>
2019-09-30 15:55:51 +0100

    User can create a project
    Set a service name. Added basic routes. Added views for creating a project, creating an idea and viewing project info.
    
    TODO: Output created values

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/share-project/create-project', function(req, res) {
+  res.render('share-project/create-project');
+});
+
+router.get('/share-idea/create-idea', function(req, res) {
+  res.render('share-idea/create-idea');
+});
+
+router.get('/projects/', function(req, res) {
+  res.render('projects/index');
+});
+
 module.exports = router
